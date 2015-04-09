f4c382cc80fff925542f783624e4e6aaeebd1e8c
Cameron Henlin <cam.henlin@gmail.com>
2015-04-08 17:38:48 -0700

    started adding route stuff

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,6 +2,14 @@
 /*!
  * Module dependencies.
  */
+var Activity          = mongoose.model('Activity');
+var Actor             = mongoose.model('Actor');
+var ActorVerbActivity = mongoose.model('ActorVerbActivity');
+var Context           = mongoose.model('Context');
+var Definition        = mongoose.model('Definition');
+var Result            = mongoose.model('Result');
+var User              = mongoose.model('User');
+var Verb              = mongoose.model('Verb');
 
 exports.index = function (req, res) {
   res.render('home/index', {
