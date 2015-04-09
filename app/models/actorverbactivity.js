0136661fdf9e902f7ccc7ea0299bf6b1cb1a2436
Cameron Henlin <cam.henlin@gmail.com>
2015-04-08 17:18:04 -0700

    built out models some more

--- app/models/actorverbactivity.js
+++ app/models/actorverbactivity.js
@@ -14,7 +14,8 @@ var ActorVerbActivitySchema = new Schema({
   actor: { type: Object, default: null },
   verb: { type: Object, default: null },
   object: { type: Object, default: null },
-  context: { type: Object, default: null }
+  context: { type: Object, default: null },
+  result: { type: Object, default: null }
 });
 
 /**
