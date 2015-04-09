d0fcf0aae4c652be758b58e6951ccaa16f96917e
Cameron Henlin <cam.henlin@gmail.com>
2015-04-08 18:01:49 -0700

    stubbed out more controller stuff

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -77,3 +77,33 @@ exports.getVerb = function (req, res, next) {
   var verb = new Verb();
 };
 
+exports.updateActivity = function (req, res, next) {
+  var activity = new Activity();
+};
+
+exports.updateActor = function (req, res, next) {
+  var actor = new Actor();
+};
+
+exports.updateActorVerbActivity = function (req, res, next) {
+  var actorVerbActivity = new ActorVerbActivity();
+
+  res.send(200, actorVerbActivity);
+};
+
+exports.updateContext = function (req, res, next) {
+  var context = new Context();
+};
+
+exports.updateDefinition = function (req, res, next) {
+  var definition = new Definition();
+};
+
+exports.updateResult = function (req, res, next) {
+  var result = new Result();
+};
+
+exports.updateVerb = function (req, res, next) {
+  var verb = new Verb();
+};
+
